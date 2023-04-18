6c2eb8380bd374da6284f88680c305d7854ca0a2
htoloi <113933950+htoloi@users.noreply.github.com>
2023-04-18 09:37:07 +0300

    C

--- _config.js
+++ _config.js
@@ -4,6 +4,6 @@ var config = {}
 config.mongoURI = {
     production: 'mongodb+srv://<toloih>:<yzdxqvijFszvyoDA>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://<toloih>:<yzdxqvijFszvyoDA>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    test: 'mongodb+srv://toloih:Volvos40@htcluster.ltzaolu.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
